16e4fa6fc2b56b9cb7d1d4eb8ebb550c7faabe8a
MaroBytes <MaroBytes@aliyun.com>
2022-04-16 19:25:07 +0800

    update: 🎯 daily code

---
+++ code_4_16.py
@@ -0,0 +1,28 @@
+"""
+Daily Code of Python
+"""
+
+# 文件操作
+# import os
+# import time
+
+# t1 = time.time()
+# for i in range(1, 3):
+#     CI = str(i)
+#     # os.mkdir(ci)
+#     os.rmdir(CI)
+# t2 = time.time()
+# print(t2 - t1) # 计算文件夹建立时间
+
+# import os
+# f = open(r'D:\\test.txt', 'w+')
+# f.write('Python')
+# f.seek(0)
+# m = f.read(3)
+# print(m)
+# f.close()
+# os.remove("D:\\test.txt")
+
+# import pdb
+# m = dir(pdb)
+# print(m)
